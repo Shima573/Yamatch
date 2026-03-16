a6cc2c8ede897ec7351044d8ab873177b57a8f8e
島拓海 <shimatakumi@shimatakuminoiMac.local>
2026-03-16 16:37:18 +0900

    userモデルに診断結果との関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   # 名前を必須にし、50文字以内にする
   validates :name, presence: true, length: { maximum: 50 }
+
+  # 診断結果との関連付け（1対多）
+  has_many :diagnoses, dependent: :destroy
 end
