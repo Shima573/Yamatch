98c8aee2be44a10c28569646d233304e778d7cd2
島拓海 <shimatakumi@shimatakuminoiMac.local>
2026-03-23 14:30:35 +0900

    ログイン後のマイページ実装

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,13 @@
+class ProfilesController < ApplicationController
+  # ログインしていない場合はログイン画面へ飛ばす
+  before_action :authenticate_user!
+
+  def show
+    # resources :profiles の場合は params[:id] が必要ですが、
+    # マイページとして使う場合は current_user を基本にします
+    @user = current_user
+
+    # 今後、診断履歴（@diagnoses）や
+    # お気に入り（@favorite_mountains）をここで取得するように拡張できる
+  end
+end
