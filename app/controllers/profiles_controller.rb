4cedd61b8d596efb5256cd91b22c96eb59c8abfd
島拓海 <shimatakumi@shimatakuminoiMac.local>
2026-04-02 16:24:20 +0900

    マイページの仮レイアウト作成完了

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,5 +9,12 @@ class ProfilesController < ApplicationController
 
     # 今後、診断履歴（@diagnoses）や
     # お気に入り（@favorite_mountains）をここで取得するように拡張できる
+    @diagnosis = current_user.diagnoses.last
+  end
+
+  private
+
+  def 
+    
   end
 end
