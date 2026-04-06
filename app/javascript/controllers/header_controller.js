2e455341a7f23bc3adecf1fa8124ff7eeaefd47d
島拓海 <shimatakumi@shimatakuminoiMac.local>
2026-04-06 23:32:38 +0900

    ヘッダーのナビアイコンのレスポンス化の実装

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="header"
 export default class extends Controller {
+  static targets = ["menu"]
+
   connect() {
     const header = this.element
     addEventListener("scroll", () => {
@@ -11,5 +13,10 @@ export default class extends Controller {
         header.classList.remove("is-scrolled");
       }
     });
+  }
+
+  toggle() {
+    this.element.classList.toggle("is-open")
+    this.menuTarget.classList.toggle("hidden")
   }
 }
