f533749848bdefcfe4e86c24a3de85762f407745
島拓海 <shimatakumi@shimatakuminoiMac.local>
2026-05-21 18:57:56 +0900

    登山ログの削除機能を追加、あわせてフラッシュメッセージの共通化とレイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,5 @@ Rails.application.routes.draw do
     resource :favorite, only: [ :create, :destroy ]
   end
   resources :favorites, only: [ :index ]
-  resources :activity_records, only: [ :index, :new, :create, :show, :edit, :update ]
+  resources :activity_records, only: [ :index, :new, :create, :show, :edit, :update, :destroy ]
 end
