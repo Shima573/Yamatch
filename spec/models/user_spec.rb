da89f456b5cd8808bad244193e59db7c1f2b56f9
島拓海 <shimatakumi@shimatakuminoiMac.local>
2026-06-03 18:12:45 +0900

    Userモデルの名前に関するバリデーション（必須・文字数制限）のテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe User, type: :model do
     user = build(:user)
     expect(user).to be_valid
   end
+
+  it '名前がない場合は無効であること' do
+    user = build(:user, name: nil)
+    expect(user).not_to be_valid
+  end
+
+  it '名前が51文字以上の場合は無効であること' do
+    user = build(:user, name: 'a' * 51)
+    expect(user).not_to be_valid
+  end
 end
