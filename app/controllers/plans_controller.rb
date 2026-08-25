283f644e0e56d6842173e8c0eeb20d77fb9a072e
島拓海 <shimatakumi@shimatakuminoiMac.local>
2026-08-25 21:26:08 +0900

    登山プランの保存処理を実装

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -5,5 +5,24 @@ class PlansController < ApplicationController
   end
 
   def create
+    @mountain = Mountain.find(params[:mountain_id])
+    @plan = current_user.plans.new(plan_params)
+    # PlanはこのMountainに所属と関連付けを設定
+    @plan.mountain = @mountain
+    # 保存する対象はPlan
+    @plan.save
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(
+      :title,
+      :climbing_date,
+      :companion_count,
+      :route,
+      :note,
+      equipment: [],
+    )
   end
 end
